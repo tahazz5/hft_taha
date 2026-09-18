6f3c9f4d851e84fb20e6900f5660e191a3f904a3
tahazz5 <130492908+tahazz5@users.noreply.github.com>
2026-09-18 18:56:53 +0200

    chore: remove generated build artifacts

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leo_taha/taha_arc_cpp
-# Build directory: /home/leo_taha/taha_arc_cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(orderbook_tests "/home/leo_taha/taha_arc_cpp/build/orderbook_tests")
-set_tests_properties(orderbook_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/leo_taha/taha_arc_cpp/CMakeLists.txt;14;add_test;/home/leo_taha/taha_arc_cpp/CMakeLists.txt;0;")
