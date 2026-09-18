5149795134e5e444e0f2cb21c296a31d97a98f51
tahazz5 <130492908+tahazz5@users.noreply.github.com>
2026-09-18 18:38:01 +0200

    chore: remove tracked build artifacts

--- build/benchmarks/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leo_taha/taha_arc_cpp/benchmarks
-# Build directory: /home/leo_taha/taha_arc_cpp/build/benchmarks
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
